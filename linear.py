481848b6a39cb99c1531194730ab9dc2078ceaf9
sergiojmnz <61261358+sergiojmnz@users.noreply.github.com>
2022-12-06 19:01:34 -0600

    se modifico linear.py

--- linear.py
+++ linear.py
@@ -10,12 +10,11 @@ print(tf.__version__)
 
 X = np.arange(-10.0, 10.0, 1e-2)
 np.random.shuffle(X)
-y =  10.0 * X + 0.0
+y = X / 28.317
 
 train_end = int(0.6 * len(X))
-#print (train_end)
 test_start = int(0.8 * len(X))
-#print (test_start)
+
 X_train, y_train = X[:train_end], y[:train_end]
 X_test, y_test = X[test_start:], y[test_start:]
 X_val, y_val = X[train_end:test_start], y[train_end:test_start]
@@ -28,7 +27,7 @@ linear_model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.m
 print(linear_model.summary())
 
 linear_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=20)
-print(linear_model.predict([ [0.0], [2.0], [3.1], [4.2], [5.2] ] ).tolist() )   
+print(linear_model.predict([ [1.0], [6.0], [4.7], [7.4], [5.5] ] ).tolist() )   
 
 export_path = 'linear-model/1/'
 tf.saved_model.save(linear_model, os.path.join('./',export_path))
